eee23ad7837b1a7215db95dcd841d963604a71e5
yuvsag <yuvalsgmn@gmail.com>
2020-05-19 07:46:26 +0300

    drive work so far
    not done yet

--- PlayVideo/app/src/main/java/com/example/playvideo/MainActivity.java
+++ PlayVideo/app/src/main/java/com/example/playvideo/MainActivity.java
@@ -1,22 +1,34 @@
 package com.example.playvideo;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.widget.VideoView;
 
 import static android.view.View.SYSTEM_UI_FLAG_FULLSCREEN;
 import static android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
@@ -33,41 +45,11 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String APPLICATION_NAME = "Google Drive API Java Quickstart";
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-    /**
-     * Global instance of the scopes required by this quickstart.
-     * If modifying these scopes, delete your previously saved tokens/ folder.
-     */
-    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_METADATA_READONLY);
-    private static final String CREDENTIALS_FILE_PATH = "/credentials_android.json";
 
 
-    /**
-     * Creates an authorized Credential object.
-     * @param HTTP_TRANSPORT The network HTTP Transport.
-     * @return An authorized Credential object.
-     * @throws IOException If the credentials.json file cannot be found.
-     */
-    private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-        // Load client secrets.
-        //InputStream in = MainActivity.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        InputStream in = getResources().openRawResource(R.raw.credentials_android);
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-        }
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                //.setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-    }
+
+
 
 
 
@@ -79,49 +61,108 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        VideoView mVideoView = findViewById(R.id.videoview);
-        mVideoView.setSystemUiVisibility(SYSTEM_UI_FLAG_FULLSCREEN | SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        String driveApiKey = "AIzaSyBBgrjCtCIfgk9XmhWzgKLo_SQTC8_MNRg";
-        String shareURL = "https://drive.google.com/file/d/15jzXnAZDTAqw1z_DoNREXmMhFAgjcV2O/view?usp=sharing";
-        String url = "https://www.googleapis.com/drive/v3/files/" + shareURL.substring(32, shareURL.length() - 17) + "?alt=media&key="+driveApiKey;
+
+        Log.d("0","hi");
+
+        requsetSingIn();
+        //VideoView mVideoView = findViewById(R.id.videoview);
+        //mVideoView.setSystemUiVisibility(SYSTEM_UI_FLAG_FULLSCREEN | SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        //String driveApiKey = "AIzaSyBBgrjCtCIfgk9XmhWzgKLo_SQTC8_MNRg";
+        //String shareURL = "https://drive.google.com/file/d/15jzXnAZDTAqw1z_DoNREXmMhFAgjcV2O/view?usp=sharing";
+        //String url = "https://www.googleapis.com/drive/v3/files/" + shareURL.substring(32, shareURL.length() - 17) + "?alt=media&key="+driveApiKey;
         //Uri uri = Uri.parse(url);
-        Uri uri = Uri.parse("https://www.drivehq.com/file/DFPublishFile.aspx/FileID7155480515/Keyovdybccfaog2/WIN_20200515_16_24_36_Pro.mp4");
-                            // https://www.drivehq.com/file/DFPublishFile.aspx/FileID7155513124/Keyu0gbwenen86n/Camera3_18-16-08.mp4
-        mVideoView.setVideoURI(uri);
-        mVideoView.start();
+        //Uri uri = Uri.parse(shareURL);
 
-/*
+        //mVideoView.setVideoURI(uri);
+        //mVideoView.start();
+
+
         // Build a new authorized API client service.
-        final NetHttpTransport HTTP_TRANSPORT;
-        try {
-            //HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            HTTP_TRANSPORT = new com.google.api.client.http.javanet.NetHttpTransport();
-            Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                    .setApplicationName(APPLICATION_NAME)
-                    .build();
 
-
-            // Print the names and IDs for up to 10 files.
-            FileList result = service.files().list()
-                    .setPageSize(100)
-                    .setQ("mimeType contains 'image/'")
-                    .setFields("nextPageToken, files(id, name,mimeType,webViewLink,properties)")
-                    .execute();
-            List<File> files = result.getFiles();
-            if (files == null || files.isEmpty()) {
-                System.out.println("No files found.");
-            } else {
-                System.out.println("Files:");
-                for (File file : files) {
-                    System.out.printf("%s | %s | %s | %s \n", file.getName(), file.getId(),file.getMimeType(), 	file.getWebViewLink());
-                }
-            }
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
 
 
     }
+
+    private void requsetSingIn()
+    {
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestEmail()
+        .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
+        .build();
+
+
+        GoogleSignInClient client =  GoogleSignIn.getClient(this,signInOptions);
+
+        startActivityForResult(client.getSignInIntent(),400);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode)
+        {
+            case 400:
+                if(resultCode == RESULT_OK)
+                {
+                    handleSignInIntent(data);
+                }
+                break;
+        }
+
+    }
+
+    private void handleSignInIntent(Intent data) {
+
+        GoogleSignIn.getSignedInAccountFromIntent(data)
+                .addOnSuccessListener(new OnSuccessListener<GoogleSignInAccount>() {
+                    @Override
+                    public void onSuccess(GoogleSignInAccount googleSignInAccount) {
+                        GoogleAccountCredential credential =  GoogleAccountCredential
+                                .usingOAuth2(MainActivity.this,Collections.singleton(DriveScopes.DRIVE_FILE));
+                        credential.setSelectedAccount(googleSignInAccount.getAccount());
+
+                        try {
+                            //HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+                            Drive service = new Drive.Builder(
+                                    AndroidHttp.newCompatibleTransport(),
+                                    new GsonFactory(),
+                                    credential)
+                                    .setApplicationName("playvideo")
+                                    .build();
+
+
+
+
+                            // Print the names and IDs for up to 10 files.
+                            FileList result = service.files().list()
+                                    .setPageSize(100)
+                                    .setQ("mimeType contains 'text/'")
+                                    .setFields("nextPageToken, files(id, name,mimeType,webViewLink,properties)")
+                                    .execute();
+                            List<File> files = result.getFiles();
+                            if (files == null || files.isEmpty()) {
+                                System.out.println("No files found.");
+                            } else {
+                                System.out.println("Files:");
+                                for (File file : files) {
+                                    Log.d("0",file.getName()+"||" +file.getId()+"||"+ file.getMimeType());
+                                }
+                            }
+
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+    }
 }
+
